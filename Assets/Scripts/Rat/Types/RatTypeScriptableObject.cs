ee405b7e191b5a0110e86d26354bbc3b7d61b3fe
shufflesninja <isaact23@vt.edu>
2021-11-06 14:02:56 -0400

    Add modifiers to RatTypeScriptableObject

--- Assets/Scripts/Rat/Types/RatTypeScriptableObject.cs
+++ Assets/Scripts/Rat/Types/RatTypeScriptableObject.cs
@@ -7,9 +7,18 @@ namespace Rat
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/RatTypeScriptableObject", order = 2)]
     public class RatTypeScriptableObject : ScriptableObject
     {
+        // Evolution
         public RatTypeScriptableObject[] nextGeneration;
+        
+        // Appearance
+        public Color color;
         public Sprite sprite;
+        
+        // Attributes
         public float startHp;
         public float speed;
+        public float defense;
+        public float bombDefense;
+        public float magicDefense;
     }
 }
