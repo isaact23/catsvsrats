da7f6af1fd72b78785de079a45dc4846d1b7208b
shufflesninja <isaact23@vt.edu>
2021-11-07 16:40:55 -0500

    Commit

---
+++ Assets/SpriteAnimation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAnimation : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float spritesPerSec;
+
+    private Image image;
+    private float timeElapsed = 0f;
+    private int currentSprite;
+
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        if (sprites.Length > 0) {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > 1 / spritesPerSec) {
+                timeElapsed = 0f;
+                currentSprite++;
+                if (currentSprite >= sprites.Length) {
+                    currentSprite = 0;
+                }
+            }
+
+            image.sprite = sprites[currentSprite];
+        }
+    }
+}
