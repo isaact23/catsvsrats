a9baf5bd06fe91100dffdcb77f0a7213fa336d1e
ethicalAndroid <mateocdelarosa@gmail.com>
2021-11-07 12:04:12 -0500

    Changing Cats and working on main Menu

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -2,32 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject credits;
     public GameObject tutorial;
+    private bool loading;
+    void Start()
+    {
+        loading = false;
+    }
 
     public void OnStartButtonPress()
     {
         // LOAD OPENING SCENE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!?
+        loading = true;
     }
 
     public void OnCreditsButtonPress()
     {
+        if (loading)
+        {
+            return;
+        }
         mainMenu.SetActive(false);
         credits.SetActive(true);
     }
 
     public void OnTutorialButtonPress()
     {
+        if (loading)
+        {
+            return;
+        }
         mainMenu.SetActive(false);
         tutorial.SetActive(true);
     }
     
     public void OnBackButtonPress()
     {
+        if (loading)
+        {
+            return;
+        }
         mainMenu.SetActive(true);
         credits.SetActive(false);
         tutorial.SetActive(false);
