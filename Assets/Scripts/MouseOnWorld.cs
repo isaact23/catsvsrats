4aebdc1784e4c4fd12f8ec87266562d481b2843d
ethicalAndroid <mateocdelarosa@gmail.com>
2021-11-06 21:15:47 -0400

    Creating in SampleScene2

--- Assets/Scripts/MouseOnWorld.cs
+++ Assets/Scripts/MouseOnWorld.cs
@@ -7,7 +7,6 @@ public class MouseOnWorld : MonoBehaviour
     public Vector3 mousePos = Vector3.zero;
     Camera cam;
     public Transform testObject;
-    public Placeable toPlace;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +16,7 @@ public class MouseOnWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
-        testObject.position = mousePos;
-        if (true || toPlace != null) 
-        {
-            Vector3 placePos = new Vector3(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y), 0f);
-            testObject.position = placePos;
-        }
-
-
-    }
-
-    private bool CanSelectCat()
-    {
-        return true;
+        testObject.position = GetPlacerPosition();
     }
     public Vector3 GetPlacerPosition()
     {
