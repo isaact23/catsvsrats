2fb12f2e56123ca2f47b68b6155dd186f2627b25
ethicalAndroid <mateocdelarosa@gmail.com>
2024-03-04 19:51:33 -0500

    (forgot to push the final update for webgl)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rat;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public RatManager ratManager;
+    public int health = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ratManager.StartRound(1);
+    }
+}
